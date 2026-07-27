a841d05f009ce145eeeb373221085e0d764070fb
Joel Vasquez <joelvasquezospino11@gmail.com>
2026-07-27 16:56:24 -0500

    build: scaffold backend architecture

---
+++ backend/src/main/java/com/magyen/platform/MagyenPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.magyen.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagyenPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagyenPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/magyen/platform/MagyenPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magyen.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagyenPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
